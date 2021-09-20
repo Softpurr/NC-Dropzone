ffe2f51f97fb2513936a1500ba6ed3e950cf3ad7
Softpurr <chakatsoftpurr@hotmail.com>
2021-09-20 16:20:42 +0200

    main

---
+++ boot/src/test/java/nc_dropzone_boot/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package nc_dropzone_boot.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
