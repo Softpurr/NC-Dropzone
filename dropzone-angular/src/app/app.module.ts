5d453090cccb446d0e4c1ab53cbbd73d27a864a4
Softpurr <chakatsoftpurr@hotmail.com>
2021-09-20 16:54:25 +0200

    components

--- dropzone-angular/src/app/app.module.ts
+++ dropzone-angular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VolComponent } from './admin/vol/vol.component';
+import { SautComponent } from './admin/saut/saut.component';
+import { ParachuteComponent } from './admin/parachute/parachute.component';
+import { ParachutisteComponent } from './admin/parachutiste/parachutiste.component';
+import { PiloteComponent } from './admin/pilote/pilote.component';
+import { AvionComponent } from './admin/avion/avion.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VolComponent,
+    SautComponent,
+    ParachuteComponent,
+    ParachutisteComponent,
+    PiloteComponent,
+    AvionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
