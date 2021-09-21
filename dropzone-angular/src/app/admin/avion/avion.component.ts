96a1797a50954a89f525fe5353096c6d6ceb1552
Softpurr <chakatsoftpurr@hotmail.com>
2021-09-21 11:47:54 +0200

    avionadmin

--- dropzone-angular/src/app/admin/avion/avion.component.ts
+++ dropzone-angular/src/app/admin/avion/avion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AvionService } from 'src/app/avion.service';
 
 @Component({
   selector: 'app-avion',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvionComponent implements OnInit {
 
-  constructor() { }
+  avions: any = [];
+  avionForm: any = {
+    nomAvion:"",
+    capaciteTransport:0,
+    rotationMax:0,
+    isDispo: false
+  };
+
+  refresh = () => {
+    this.avions = this.serviceAvion.findAll();
+  }
+
+  supprimerAvion(avion: any){
+    this.serviceAvion.delete(avion).subscribe(this.refresh);
+  }
+  editerAvion(avion: any){
+    this.avionForm = avion;
+  }
+  modifierAvion(){
+    this.serviceAvion.update(this.avionForm).subscribe(this.refresh);
+  }
+  ajouterAvion(){
+    this.serviceAvion.add(this.avionForm).subscribe(this.refresh);
+  }
+
+  constructor(private serviceAvion: AvionService) { 
+    this.avions = this.serviceAvion.findAll();
+  }
 
   ngOnInit(): void {
   }
+
 
 }
