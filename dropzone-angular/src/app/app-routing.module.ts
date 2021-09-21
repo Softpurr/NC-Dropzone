0a5d2c77f5e80e37551fa96b16379f4dbb30e784
MickaAile <ringuet-mickael@wanadoo.fr>
2021-09-21 11:50:49 +0200

    nav

--- dropzone-angular/src/app/app-routing.module.ts
+++ dropzone-angular/src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvionComponent } from './admin/avion/avion.component';
+import { ParachuteComponent } from './admin/parachute/parachute.component';
+import { ParachutisteComponent } from './admin/parachutiste/parachutiste.component';
+import { PiloteComponent } from './admin/pilote/pilote.component';
+import { SautComponent } from './admin/saut/saut.component';
+import { VolComponent } from './admin/vol/vol.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [/*
+const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'enregistrement', component: EnregistrementComponent },
-  { path: 'planning', component: PlanningComponent },
-  { path: 'connexion', component: ConnexionComponent },*/
+  { path: 'avion', component: AvionComponent },
+  { path: 'parachute', component: ParachuteComponent },
+  { path: 'parachutiste', component: ParachutisteComponent },
+  { path: 'pilote', component: PiloteComponent },
+  { path: 'saut', component: SautComponent },
+  { path: 'vol', component: VolComponent },
+  // { path: 'enregistrement', component: EnregistrementComponent },
+  // { path: 'planning', component: PlanningComponent },
+  // { path: 'connexion', component: ConnexionComponent },
 ];
 
 @NgModule({
