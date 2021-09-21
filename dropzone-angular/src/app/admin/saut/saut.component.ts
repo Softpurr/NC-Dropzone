341c6a059d293a357258245dfd13bd5e34dee12a
Zafiria <stephen.vanspeybrouck@gmail.com>
2021-09-21 11:21:01 +0200

    formPrachutiste

--- dropzone-angular/src/app/admin/saut/saut.component.ts
+++ dropzone-angular/src/app/admin/saut/saut.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ParachuteService } from 'src/app/parachute.service';
+import { ParachutisteService } from 'src/app/parachutiste.service';
+import { SautService } from 'src/app/saut.service';
+import { VolService } from 'src/app/vol.service';
 
 @Component({
   selector: 'app-saut',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SautComponent implements OnInit {
 
-  constructor() { }
+  constructor(private srvSaut : SautService, private srvVol : VolService, private srvParachutiste : ParachutisteService, private srvParachute : ParachuteService) { 
+
+    this.parachutistes = this.srvParachutiste.findAll();
+    this.parachutes = this.srvParachute.findAll();
+
+  }
 
   ngOnInit(): void {
   }
 
+  formParachutiste = {
+    nom: "", prenom: "", numero_licence: "", date: 21/09/2021
+  }
+  formParachute = {
+  }
+  formVol = {
+
+  }
+
+  parachutistes: any = [];
+  parachutes: any = [];
 }
