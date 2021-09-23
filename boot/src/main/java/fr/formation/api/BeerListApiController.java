7fae55fdf4208c2baeb02a5edd5447979e2b60e1
Zafiria <stephen.vanspeybrouck@gmail.com>
2021-09-23 16:22:13 +0200

    beerlist

--- boot/src/main/java/fr/formation/api/BeerListApiController.java
+++ boot/src/main/java/fr/formation/api/BeerListApiController.java
@@ -5,16 +5,22 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.formation.dao.IBeerListDaoJpaRepository;
 import fr.formation.model.BeerList;
 
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/beerlist")
 public class BeerListApiController {
     
     @Autowired
